a8347ca58ab4f0994ededf56d9c3ad3554367b72
Thien Truong <truongtichthien0401@gmail.com>
2022-07-28 12:09:45 +0700

    Implement the Share page

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,3 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {joinCls, validateYouTubeUrl} from '../../utilities';
+import cls from './Share.module.scss';
 
-export default () => <>Share</>;
+export default () => {
+  const [url, setUrl] = useState('');
+  const [validUrl, setValidUrl] = useState(false);
+
+  const handleUrlChange = (e) => {
+    const val = e.target.value;
+    setUrl(val);
+  }
+
+  const handleShare = () => {
+  };
+
+  useEffect(() => {
+    setValidUrl(validateYouTubeUrl(url));
+  }, [url]);
+
+  return (
+    <div className={cls.appShare}>
+      <fieldset className={cls.boundary}>
+        <legend className={cls.legend}>Share a YouTube video</legend>
+        <div className={cls.urlForm}>
+          <label htmlFor="youtube-url"
+                 className={joinCls([cls.label, !!url && !validUrl && cls.error])}>YouTube URL:</label>
+          <input type="text"
+                 id="youtube-url"
+                 className={cls.input}
+                 value={url}
+                 onChange={handleUrlChange}/>
+        </div>
+        <div className={joinCls([cls.msg, cls.error])}>{!!url && !validUrl && 'YouTube URL is invalid!'}</div>
+        <button type="button"
+                className={joinCls([cls.btn, cls.primaryBtn, !validUrl && cls.disabled])}
+                onClick={handleShare}>Share
+        </button>
+      </fieldset>
+    </div>
+  );
+};
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,1 +1,10 @@
 export const joinCls = (array) => array.filter((e) => !!e).join(' ');
+
+export const validateYouTubeUrl = (url) => {
+  const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  const match = url.match(regExp);
+  if (match && match[1].length === 11) { // YouTube video ID is almost 11 characters
+    return match[1]
+  }
+  return false;
+}
